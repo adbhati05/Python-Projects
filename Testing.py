2a22fe4785f8156b89e11ca3eac263e1819d3a55
Aditya Bhati <adbhati75@gmail.com>
2024-08-15 19:57:20 -0500

    Edited Testing.

--- Testing.py
+++ Testing.py
@@ -1,6 +1,6 @@
 name = input("Who is the patient being entered into the system? ")
 age =  input("How old is the patient? ")
-status = input("What is the current condition of the patient (EM = Emaciated, EX = Exhausted, IP = In Pain? ")
+status = input("What is the current condition of the patient? (EM = emaciated, EX = exhausted, IP = in pain, MU = mentally unstable) ")
 procedure = "Sign patient out of hospital."
 
 if status == "EM":
@@ -12,6 +12,8 @@ elif status == "EX":
 elif status == "IP":
     procedure = "Have nurse place patient on painkillers and prepare them to rest."
     status = "Currently in pain"
+elif status == "MU":
+    procedure = "Inform local psychiatric hospital and immediately have patient registered into their system for assistance."
 
 print("Patient name: " + name)
 print("Patient age: " + str(age))
